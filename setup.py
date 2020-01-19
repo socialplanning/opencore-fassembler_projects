18f2fb4dfdfcb6b6f30d50b2dc169e24b114ce2e
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2020-01-19 15:13:37 -0400

    Updated metadata classifiers strings for this package

--- setup.py
+++ setup.py
@@ -3,21 +3,37 @@ import sys, os
 
 version = '0.5'
 
+long_description = (
+    open("README.rst").read()
+    + '\n' +
+    'Changes\n'
+    '=======\n'
+    + '\n' +
+    open("docs/CHANGES.txt").read()
+    + '\n')
+
 setup(name='opencore-fassembler_projects',
       version=version,
       description="Assorted fassembler projects for creating repeatable OpenCore builds",
-      long_description="""
-Contains fassembler projects for building components of an OpenCore stack.
-%s
-
-Changes
-=======
-
-%s
-""" % (open("README.rst").read(),
-       open("docs/CHANGES.txt").read())
-      classifiers=[], 
-      keywords='',
+      long_description=long_description,
+      # Get strings from https://pypi.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.4",
+        "Programming Language :: Python :: 2 :: Only",
+        "Topic :: Desktop Environment :: File Managers",
+        "Topic :: Software Development :: Assemblers",
+        "Topic :: System :: Systems Administration",
+      ],
+      keywords='fassembler projects frontend zine socialplanning opencore',
       author='Ethan Jucovy',
       author_email='opencore-dev@lists.coactivate.org',
       url='http://coactivate.org/projects/opencore',
